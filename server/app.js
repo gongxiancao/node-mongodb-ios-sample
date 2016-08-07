fec59ffb07e9e4a81095e62c73c7f7185b631f9c
GongXian Cao <gongxiancao@hotmail.com>
2016-08-07 11:17:09 +0800

    add user create/find/get/update server api

--- server/app.js
+++ server/app.js
@@ -1,8 +1,60 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+  app = express(),
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/test');
+
+var User = mongoose.model('User', { name: String, email: String });
+
+app.use(bodyParser.json());
+
+app.post('/api/v1/user', function (req, res) {
+  console.log(req.url, req.body);
+  User.create(req.body)
+    .then(function (user) {
+      res.json(user);
+    })
+    .catch(function (err) {
+      res.status(400, {errcode: 1, errmsg: err.message});
+    });
+});
+
+app.post('/api/v1/user/:id', function (req, res) {
+  console.log(req.url, req.body);
+  User.update({_id: req.params.id}, req.body)
+    .then(function (user) {
+      console.log(user);
+      res.json(user);
+    })
+    .catch(function (err) {
+      res.status(400, {errcode: 1, errmsg: err.message});
+    });
+});
+
+app.get('/api/v1/user', function (req, res) {
+  console.log(req.url);
+  User.find({})
+    .lean()
+    .sort({_id: 1})
+    .then(function (users) {
+      res.json({users: users});
+    })
+    .catch(function (err) {
+      res.status(400).json({errcode: 2, errmsg: err.message});
+    });
+});
+
+app.get('/api/v1/user/:id', function (req, res) {
+  console.log(req.url);
+  User.findOne({_id: req.params.id})
+    .then(function (user) {
+      res.json(user);
+    })
+    .catch(function (err) {
+      res.status(404).json({errcode: 3, errmsg: err.message});
+    });
 });
 
 app.listen(3000, function () {
